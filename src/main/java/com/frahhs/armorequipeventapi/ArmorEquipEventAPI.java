6e6e762c649afbdf0f5940edff922783e70b1f18
Francesco De Caro <francesco.decaro.dev@gmail.com>
2023-07-07 12:53:47 +0200

    Void project added

---
+++ src/main/java/com/frahhs/armorequipeventapi/ArmorEquipEventAPI.java
@@ -0,0 +1,17 @@
+package com.frahhs.armorequipeventapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorEquipEventAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
